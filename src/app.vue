e8663a143b0f1303ac4bb8b3ee35d6ad90f27f6b
nbdyxy <yyhegsea@163.com>
2018-04-16 00:35:27 +0800

    webpack以及vue组件

---
+++ src/app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-title title="Vue组件化"></v-title>
+    <v-button @click="handleClick">点击按钮</v-button>
+  </div>
+</template>
+<script>
+  import vTitle from './components/title.vue';
+  import vButton from './components/button.vue';
+
+  export default {
+    components: {
+      vTitle,
+      vButton
+    },
+    methods: {
+      handleClick (e) {
+        console.log(e);
+      }
+    },
+    data () {
+      return {
+        color: '#f60'
+      }
+    }
+  }
+</script>
+<style scoped>
+  div{
+    color: #f60;
+    font-size: 24px;
+  }
+</style>
